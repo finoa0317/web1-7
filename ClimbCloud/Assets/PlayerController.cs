3ee222ca807840f00f75dcad3db13fc7e97f80d4
Unknown <g1727075@st.t-kougei.ac.jp>
2018-06-18 17:28:55 +0900

    前回の続き

--- ClimbCloud/Assets/PlayerController.cs
+++ ClimbCloud/Assets/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
@@ -17,7 +18,8 @@ public class PlayerController : MonoBehaviour {
 	
 
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&&
+                this.rigid2D.velocity.y==0)
         {
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
@@ -37,5 +39,16 @@ public class PlayerController : MonoBehaviour {
         }
         this.animator.speed = speedx / 2.0f;
 
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+
 	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ゴール");
+        SceneManager.LoadScene("ClearScene");
+    }
+    
 }
